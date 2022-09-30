9e5829ed56977f99fef3082c6a21e824ebe4b671
Lavanov <alavanov@rencap.com>
2022-09-30 11:10:55 +0300

    Longest Repeting Elements in Array

---
+++ Longest Repeting Elements in Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+
+Clear();
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+WriteLine("Введите количество элементов массива: ");
+int length = int.Parse(ReadLine()!);
+
+WriteLine("Введите число,у которого нужно найти максимальное количество повторений подряд: ");
+int number = int.Parse(ReadLine()!);
+
+
+int[] newArray = SetArray(length, 0, 2);
+Array.ForEach(newArray, el => Write(" " + el));
+
+WriteLine();
+WriteLine($"Максимальная последовательность равна: {LongestRepetingElementsInArray(newArray,number)}");
+
+
+
+
+int LongestRepetingElementsInArray(int[] array, int numberofLongest)
+{
+    int longest=0;
+    int newlongest=0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]==numberofLongest)
+        {
+        longest=Math.Max(newlongest,longest);
+        newlongest = array[i] == array[i - 1] ? newlongest+1:0;
+        }
+    }    
+
+    return longest+1;
+}
+
+
+
+int[] SetArray(int size, int minRnd, int maxRnd)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd);
+    }
+
+    return array;
+}
