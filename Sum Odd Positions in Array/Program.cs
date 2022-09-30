03ca08f4b63fb05d0bf490c0a93f823b346419f6
Lavanov <alavanov@rencap.com>
2022-09-30 09:41:44 +0300

    Sum Odd Positions in Array

---
+++ Sum Odd Positions in Array/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+Clear();
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+WriteLine("Введите количество элементов массива: ");
+int length = int.Parse(ReadLine()!);
+WriteLine("Введите минимальный элемент разброса массива: ");
+int min = int.Parse(ReadLine()!);
+WriteLine("Введите максимальный элемент разброса массива: ");
+int max = int.Parse(ReadLine()!);
+
+int[] newArray = SetArray(length, min, max);
+Array.ForEach(newArray, el => Write(" " + el));
+
+WriteLine();
+WriteLine($"Сумма нечетных позиций элементов массива равна: {SumOddPositionsInArray(newArray)} ");
+
+
+int SumOddPositionsInArray(int[] array)
+{
+    int sumOddIndex = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+        sumOddIndex = sumOddIndex + array[i];
+
+    return sumOddIndex;
+}
+
+
+int[] SetArray(int size, int minRnd, int maxRnd)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd);
+    }
+
+    return array;
+}
